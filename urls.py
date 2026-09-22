ff03fdb82c64e483a94eb0fcbe5d99039cfe6b8c
James C <99674552+ktechonline@users.noreply.github.com>
2026-09-22 11:29:57 -0400

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import cert_views as cert
+
+urlpatterns = [
+  path('cert/gen/<str:unit_type>/<int:qty>/', cert.cert_gen, name='cert_gen'),
+  path('cert/val/', cert.cert_val, name='cert_val'),
+]
